bfede2ed9c7c5a52fdf5f4f7651682904984044c
eruditeme <evangelineyiqin@gmail.com>
2022-09-07 13:09:34 -0400

    Added boundaries

--- index.js
+++ index.js
@@ -27,21 +27,17 @@ class Bounds {
     }
 
     draw() {
-        context.fillStyle = "green"
+        context.fillStyle = "rgba(255, 0, 0, 0)"
         context.fillRect(this.x, this.y, this.width, this.height)
     }
 }
 
 const boundary = []
-const offset_c = {
-    x: 0,
-    y: -175
-}
 
 cBlock.forEach((row, i) => {
     row.forEach((symbol, j) => {
         if (symbol == 2221) {
-            boundary.push(new Bounds(j * 52.8 + 0, i * 52.8 - 175))
+            boundary.push(new Bounds(j * 52.8, i * 52.8 - 175))
         }
     })
 })
@@ -76,7 +72,7 @@ class Sprite {
 
 
 const background = new Sprite(0, -175, image)
-const player = new Sprite(canvas.width/2 - 110, canvas.height/2 + 60, playerImg, 4)
+const player = new Sprite(canvas.width/2 - 110, canvas.height/2 + 80, playerImg, 4)
 
 const keys = {
     a:{pressed: false},
@@ -85,27 +81,99 @@ const keys = {
     d:{pressed: false},
 }
 
-const block = [background]
+function right(pc, blockk, block_x, block_y) {
+    return (
+        pc.x + pc.width >= block_x &&
+        pc.x <= block_x + blockk.width &&
+        pc.y <= block_y + blockk.height &&
+        pc.y + pc.height >= block_y
+    )
+}
+
+const block = [background, ...boundary]
+
+function rectangularCollision({rect1, rect2}) {
+    return (
+        rect1.x + rect1.width >= rect2.x &&
+        rect1.x <= rect2.x + rect2.width &&
+        rect1.y <= rect2.y + rect2.height &&
+        rect1.y + rect1.height >= rect2.y
+    )
+}
 
 function animate() {
     window.requestAnimationFrame(animate)
     background.draw()
-    //boundary.forEach(boundary => {boundary.draw()})
+    boundary.forEach((b) => {
+        b.draw()
+    })
     player.draw()
-    //if (playerImg.x + playerImg.width >=) {}
 
     if (keys.a.pressed) {
-        block.forEach(b => {b.x += 3})
+        let moving = true
+        for (let i = 0; i < boundary.length; i++) {
+            const boundaries = boundary[i]
+            if (rectangularCollision({rect1: player, rect2: {...boundaries, x: boundaries.x + 3, y:boundaries.y}})) {
+                console.log("colliding")
+                moving=false
+                break
+            }
+        }
+        if (moving) {
+            block.forEach((movable) => {
+                movable.x += 3
+            })
+        }
     }
     else if (keys.w.pressed) {
-        block.forEach(b => {b.y += 3})
+        let moving = true
+        for (let i = 0; i < boundary.length; i++) {
+            const boundaries = boundary[i]
+            if (rectangularCollision({rect1: player, rect2: {...boundaries, x: boundaries.x, y:boundaries.y + 3}})) {
+                console.log("colliding")
+                moving=false
+                break
+            }
+        }
+        if (moving) {
+            block.forEach((movable) => {
+                movable.y += 3         
+            })
+        }
     }
     else if (keys.s.pressed) {
-        block.forEach(b => {b.y -= 3})
+        let moving = true
+        for (let i = 0; i < boundary.length; i++) {
+            const boundaries = boundary[i]
+            if (rectangularCollision({rect1: player, rect2: {...boundaries, x: boundaries.x, y:boundaries.y - 3}})) {
+                console.log("colliding")
+                moving=false
+                break
+            }
+        }
+        if (moving) {
+            block.forEach((movable) => {
+                movable.y -= 3
+            })
+        }
     }
     else if (keys.d.pressed) {
-        block.forEach(b => {b.x -= 3})
+        let moving = true
+        for (let i = 0; i < boundary.length; i++) {
+            const boundaries = boundary[i]
+            if (rectangularCollision({rect1: player, rect2: {...boundaries, x: boundaries.x - 3, y:boundaries.y}})) {
+                console.log("colliding")
+                moving=false
+                break
+            }
+        }
+        if (moving) {
+            block.forEach((movable) => {
+                movable.x -= 3
+            })
+        }
     }
+
 }
 
 animate()
